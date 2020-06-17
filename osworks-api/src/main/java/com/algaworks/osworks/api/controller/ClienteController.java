9d45b7ef882a5b34c575bbfcd744a6fc54ea5dda
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-17 12:44:12 -0300

    (#5) Listando os clientes utilizando repository

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,24 +1,41 @@
 package com.algaworks.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
-	
-	@PersistenceContext
-	private EntityManager manager;
 
-	@GetMapping("/clientes")
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@GetMapping
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		//return clienteRepository.findByNome("teste");
+		//return clienteRepository.findByNomeContaining("a");
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{clienteid}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteid) {
+		
+		Optional<Cliente> cliente =  clienteRepository.findById(clienteid);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 }
