8c3af403a2cbe3f59acae546c55bac4abaa9c368
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-17 12:18:12 -0300

    (#5) Listando os clientes pelo banco de dados

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,23 +12,13 @@ import com.algaworks.osworks.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		
-		cliente1.setId(1L);
-		cliente1.setNome("João das neves");
-		cliente1.setTelefone("(14) 3333-3333");
-		cliente1.setEmail("joaodasneves@gmail.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria das neves");
-		cliente2.setTelefone("(14) 4444-4444");
-		cliente2.setEmail("mariadasneves@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 	
 }
