515040873fc86bab0bdf99f134748956dd9af4b5
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-19 19:10:22 -0300

    (#36) Finalizacao de ordem de servico

--- osworks-api/src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
+++ osworks-api/src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
@@ -17,37 +17,49 @@ import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 
 @Service
 public class GestaoOrdemServicoService {
-	
+
 	@Autowired
 	private OrdemServicoRepository ordemServicoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private ComentarioRepository comentarioRepository;
 
-	public OrdemServico criar (OrdemServico ordemServico) {
-		
-		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId()).
-											orElseThrow(() -> new NegocioException("Cliente não encontrado"));
-		
+	public OrdemServico criar(OrdemServico ordemServico) {
+
+		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId())
+				.orElseThrow(() -> new NegocioException("Cliente não encontrado"));
+
 		ordemServico.setCliente(cliente);
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		ordemServico.setDataAbertura(OffsetDateTime.now());
 		return ordemServicoRepository.save(ordemServico);
 	}
-	
+
+	public void finalizar(Long ordemServicoId) {
+		OrdemServico ordemServico = buscar(ordemServicoId);
+
+		ordemServico.finalizar();
+
+		ordemServicoRepository.save(ordemServico);
+	}
+
 	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
-		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
-				.orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de serviço não encontrada"));
-		
+		OrdemServico ordemServico = buscar(ordemServicoId);
+
 		Comentario comentario = new Comentario();
 		comentario.setDataEnvio(OffsetDateTime.now());
 		comentario.setDescricao(descricao);
 		comentario.setOrdemServico(ordemServico);
-		
+
 		return comentarioRepository.save(comentario);
 	}
-	
+
+	private OrdemServico buscar(Long ordemServicoId) {
+		return ordemServicoRepository.findById(ordemServicoId)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de serviço não encontrada"));
+	}
+
 }
