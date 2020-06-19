c25701a0a1e4aeb09306a13cd23608a41bcf678c
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-19 15:37:49 -0300

    (#22) Padronizando data e hora para offsetdatetime

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.model.OrdemServico;
 import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
