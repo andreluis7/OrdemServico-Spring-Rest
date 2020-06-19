ada9c3412647ce2f97289aca8b0e4f88068bdab5
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-19 17:08:14 -0300

    (#28) Criando a model de comentarios

--- osworks-api/src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
+++ osworks-api/src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
@@ -2,6 +2,8 @@ package com.algaworks.osworks.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,70 +12,96 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class OrdemServico {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@ManyToOne
 	private Cliente cliente;
-	
+
 	private String descricao;
-	
+
 	private BigDecimal preco;
 
 	@Enumerated(EnumType.STRING)
 	private StatusOrdemServico status;
-	
+
 	private OffsetDateTime dataAbertura;
-	
+
 	private OffsetDateTime dataFinalizacao;
-	
+
+	@OneToMany(mappedBy = "ordemServico")
+	private List<Comentario> comentarios = new ArrayList<>();
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Cliente getCliente() {
 		return cliente;
 	}
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public BigDecimal getPreco() {
 		return preco;
 	}
+
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
+
 	public StatusOrdemServico getStatus() {
 		return status;
 	}
+
 	public void setStatus(StatusOrdemServico status) {
 		this.status = status;
 	}
+
 	public OffsetDateTime getDataAbertura() {
 		return dataAbertura;
 	}
+
 	public void setDataAbertura(OffsetDateTime dataAbertura) {
 		this.dataAbertura = dataAbertura;
 	}
+
 	public OffsetDateTime getDataFinalizacao() {
 		return dataFinalizacao;
 	}
+
 	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
 		this.dataFinalizacao = dataFinalizacao;
 	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -81,6 +109,7 @@ public class OrdemServico {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -97,6 +126,5 @@ public class OrdemServico {
 			return false;
 		return true;
 	}
-	
-	
+
 }
